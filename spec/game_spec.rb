c624a28d2876b06c571420736e1f57f15e748055
Vakrim <vakrim92@gmail.com>
2018-01-02 18:18:55 +0100

    specs fix

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,9 +4,9 @@ describe 'game' do
   it 'runs game for 10000 steps' do
     require './maps/map_02.rb'
 
-    tick = 1/60
-    
-    10000.times {
+    tick = 1.0/60
+
+    10_000.times {
       Actor.each { |a| a.update tick }
     }
   end
